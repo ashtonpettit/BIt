e936131415781a5da5b4158527f8170c067ab1e4
Ashton Pettit <53563919+ashtonpettit@users.noreply.github.com>
2023-05-13 17:59:53 +0200

    Add files via upload

--- src/components/StartPage/spriteChoice.vue
+++ src/components/StartPage/spriteChoice.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="images">
-        <img id="spriteBlack" @click="this.showAlert(1)" src="../../assets/images/sprite.png">
-        <img id="spriteYellow"  @click="this.showAlert(2)" src="../../assets/images/sprite.png">
-        <img id="spritePink" @click="this.showAlert(3)" src="../../assets/images/sprite.png">
-        <img id="spriteGreen" @click="this.showAlert(4)" src="../../assets/images/sprite.png">
-        <img id="spriteblue" @click="this.showAlert(5)" src="../../assets/images/sprite.png">
+        <img id="nerdSprite" @click="this.showAlert('nerdSprite')" src="../../assets/images/sprite.png">
+        <img id="summerSprite"  @click="this.showAlert('summerSprite')" src="../../assets/images/sprite.png">
+        <img id="cuteSprite" @click="this.showAlert('cuteSprite')" src="../../assets/images/sprite.png">
+        <img id="cheekySprite" @click="this.showAlert('cheekySprite')" src="../../assets/images/sprite.png">
+        <img id="babySprite" @click="this.showAlert('babySprite')" src="../../assets/images/sprite.png">
     </div>
 </template>
 
@@ -12,14 +12,19 @@
 export default {
   data () {
     return {
+      previousSprite: null
 
     }
   },
   methods: {
-    showAlert(choice){
-        document.getElementById('spriteTracker').innerHTML = choice;
-
-
+    showAlert(spriteId){
+      if (this.previousSprite == null) this.previousSprite = spriteId;
+      else{
+        document.getElementById(this.previousSprite).classList.remove('active');   
+      }
+      this.previousSprite = spriteId;
+      document.getElementById(spriteId).className = 'active';
+      document.getElementById('spriteTracker').innerHTML = spriteId;
     },
 
   }
