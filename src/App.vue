5ed6c64b856bdf932e4f03165a473efdeb6dbe2c
Ashton Pettit <53563919+ashtonpettit@users.noreply.github.com>
2023-05-24 13:44:31 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <p style="display:none" id="CalculatedVolume"></p>
+  <p style="display:none" id="SessionTracker"></p>
   <p style="display: none" id="spriteTracker">0</p>
-  <p style="display: none;" id="DifficultyTracker">Easy</p>
+  <p style="display: none;" id="DifficultyTracker"></p>
 
   <!-- Sprites and MiniGame Assets -->
   <NerdSprite v-if="this.spriteID == 'nerdSprite'"></NerdSprite>
+  <SadNerdSprite v-if="this.spriteID == 'nerdSpriteSad'"></SadNerdSprite>
   <SummerSprite v-if="this.spriteID == 'summerSprite'"></SummerSprite>
   <CuteSprite v-if="this.spriteID == 'cuteSprite'"></CuteSprite>
   <CheekySprite v-if="this.spriteID == 'cheekySprite'"></CheekySprite>
   <MiniGameMedium v-if="this.currentPage == 5"></MiniGameMedium>
   <Leaderboard></Leaderboard>
-
+<div v-if="this.sessionVerify == false">
+  <button @click="this.checkSession(1)">New Session</button>
+  <button @click="this.checkSession(2)">Existing Session</button>
+</div>
   <!--Starting Pages containing instructions and Sprite Selection-->
   <div v-if="this.currentPage < 4" id="startPage-container">
 
@@ -50,23 +54,36 @@
 
 <script>
 
-import MiniGameMedium from "./components/MiniGame/minigameMedium.vue"
-import SummerSprite from "./components/Sprite/SummerSprite.vue"
-import CuteSprite from "./components/Sprite/CuteSprite.vue"
-import my_json from "./assets/testfiles/energyData.json"
+// Game Import
+import MiniGameMedium from "./components/MiniGame/minigameMedium.vue";
+
+//Sprites Happy Import
+import SummerSprite from "./components/Sprite/SummerSprite/SummerSprite.vue";
+import CuteSprite from "./components/Sprite/CuteSprite/CuteSprite.vue";
+import NerdSprite from "./components/Sprite/NerdSprite/NerdSprite.vue";
+import CheekySprite from "./components/Sprite/CheekySprite/CheekySprite.vue";
+
+//Sprites Sad Import
+import SadNerdSprite from "./components/Sprite/NerdSprite/SadNerdSprite.vue";
+
+// Components Import
+import my_json from "./assets/testfiles/energyData.json";
 import ExplainChoice from "./components/StartPage/explainChoice.vue";
 import TipsPage from "./components/StartPage/tipsPage.vue";
 import SpriteChoice from "./components/StartPage/spriteChoice.vue";
-import NerdSprite from "./components/Sprite/NerdSprite.vue";
-import CheekySprite from "./components/Sprite/CheekySprite.vue";
 import Leaderboard from "./components/Assets/leadersBoard.vue";
 
+// Session Import
+import session from "./session/session.json";
+let d = JSON.stringify(session);
+d = JSON.parse(d);
 
 export default {
   name: 'App',
   components: {
     SummerSprite,
     CuteSprite,
+    SadNerdSprite,
     MiniGameMedium,
     ExplainChoice,
     TipsPage,
@@ -78,22 +95,25 @@ export default {
   },
   data() {
     return {
-      outputToday : 0,
-      outputPrev : 0,
-      currentPage: 5,
+      currentPage: 0,
       showSprite: false,
       bitFriend: 'Milo',
+      sessionVerify: false,
       pronoun: 'him',
-      newSession: false,
-      spriteID: 'nerdSprite'
+      spriteHealth: 100,
+      spriteID: null
 
     }
   },
   mounted() {
     this.formatDates();
-    this.checkSession();
+    this.loadPage();
   },
   methods: {
+    loadPage(){
+      d.currentSession = 'true';
+
+    },
     getPrevDate(date, days){
       let day = date;
       day.setDate(day.getDate()-days);
@@ -107,30 +127,37 @@ export default {
       my_json.forEach( x => {if (x['Van Datum'] == today) if (x['Register'] == 'Afname Dag'){ todayOutput = x['Volume']}});
       my_json.forEach( x => {if (x['Van Datum'] == formatPrevDate) if (x['Register'] == 'Afname Dag'){ prevOutput = x['Volume']}});
 
-      console.log(today + " " + formatPrevDate);
-      document.getElementById("CalculatedVolume").innerHTML = todayOutput - prevOutput;
-      this.outputToday = todayOutput;
-      this.outputPrev = prevOutput;
+      console.log(formatPrevDate + " " + prevOutput);
+      console.log("Today: " + today + " " + todayOutput);
+      if (todayOutput > prevOutput){
+        document.getElementById("DifficultyTracker").innerHTML = 'Hard';
+      }
+      else {
+        document.getElementById("DifficultyTracker").innerHTML = 'Easy';
+      }
+      
     },
     formatDates(){
       let today = "18-10-2022";
       let date = today.split('-');
       let newdate = new Date(date[2] + "-" + date[1] + "-" + date[0]);
-      let prevDay = this.getPrevDate(newdate, 1).split("/");
+      let prevDay = this.getPrevDate(newdate, 365).split("/");
       let formatPrevDate = prevDay[1] + "-" + prevDay[0] + "-" + prevDay[2];
 
       //send to INIT
       this.initPage(today, formatPrevDate);
 
     },
-    checkSession(){
+    checkSession(num){
       //database connection here.
-      if (this.newSession == true){
+      if (num == 1){
         this.currentPage = 1;
-        this.newSession = false;
+        document.getElementById('sessionTracker').innerHTML = 'current';
       }
       else {
         this.currentPage = 5;
+        this.spriteID = d.sprite;
+        document.getElementById('sessionTracker').innerHTML = 'current';
       }
 
     },
